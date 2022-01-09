47ac2648bacefccb4fa6d530cdfd7f4ba06c8978
Pierre Venet <XB32Z@users.noreply.github.com>
2022-01-09 17:44:45 +0100

    Fill README with instructions and first demo
    The first demo simply dims a LED using a subscriber.

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
 # micro-ros-motor-controller
+
+This project aims to install a FreeRTOS application on an ESP32 microcontroller that allows to controll DC motors.
+
+## Prerequisites
+### Installing ROS2
+Follow this [link](https://docs.ros.org/en/foxy/Installation/Ubuntu-Install-Debians.html) for a tutorial on installing ROS2.
+
+### Installing ESPressif IDF
+Follow this [link](https://docs.espressif.com/projects/esp-idf/en/latest/esp32/get-started/) for the espressif wiki. 
+
+Note that you can also simply install the ESP IDF app in VSCode via the marketplace. 
+
+### Install micro-ros
+See also the official [tutorial](https://micro.ros.org/docs/tutorials/core/first_application_rtos/freertos/).
+``` 
+# Source the ROS 2 installation
+source /opt/ros/$ROS_DISTRO/setup.bash
+
+# Create a workspace and download the micro-ROS tools
+mkdir microros_ws
+cd microros_ws
+git clone -b $ROS_DISTRO https://github.com/micro-ROS/micro_ros_setup.git src/micro_ros_setup
+
+# Update dependencies using rosdep
+sudo apt update && rosdep update
+rosdep install --from-path src --ignore-src -y
+
+# Install pip
+sudo apt-get install python3-pip
+
+# Build micro-ROS tools and source them
+colcon build
+source install/local_setup.bash
+````
+
+## Get started
+
+This application is developped for ESP32 chips. This chip has a Wifi connection that we can use to communicate between the microcontroller and the host. We will use this in this demo but other medium can be specified (at the configure step).
+```
+cd microros_ws
+
+# Create step
+ros2 run micro_ros_setup create_firmware_ws.sh freertos esp32
+
+# Configure step
+ros2 run micro_ros_setup configure_firmware.sh motor_controller -t udp -i <local ip adress> -p 8888
+
+# If using Wifi, run this command and go to
+#  - "micro-ROS Transport Settings", then  
+#  - "WiFi Configuration" menu 
+#  - fill your WiFi SSID and password
+#  - Save your changes [S], exit the window [Q]
+
+ros2 run micro_ros_setup build_firmware.sh menuconfig
+
+# Build step 
+ros2 run micro_ros_setup build_firmware.sh
+
+# Flash step
+ros2 run micro_ros_setup flash_firmware.sh
+
+# Download micro-ROS-Agent packages
+ros2 run micro_ros_setup create_agent_ws.sh
+
+# Build step
+ros2 run micro_ros_setup build_agent.sh
+source install/local_setup.bash
+
+# Run a micro-ROS agent
+ros2 run micro_ros_agent micro_ros_agent udp4 --port 8888
+```
+
---
+++ app.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <std_msgs/msg/int32.h>
+
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#ifdef ESP_PLATFORM
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#endif
+
+#include "driver/ledc.h"
+#include "esp_err.h"
+
+#define LEDC_TIMER              LEDC_TIMER_0
+#define LEDC_MODE               LEDC_LOW_SPEED_MODE
+#define LEDC_OUTPUT_IO          (16) // Define the output GPIO
+#define LEDC_CHANNEL            LEDC_CHANNEL_0
+#define LEDC_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
+#define LEDC_DUTY               (4095) // Set duty to 50%. ((2 ** 13) - 1) * 50% = 4095
+#define LEDC_FREQUENCY          (5000) // Frequency in Hertz. Set frequency at 5 kHz
+
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
+
+
+
+static void example_ledc_init(void)
+{
+    // Prepare and then apply the LEDC PWM timer configuration
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode       = LEDC_MODE,
+        .timer_num        = LEDC_TIMER,
+        .duty_resolution  = LEDC_DUTY_RES,
+        .freq_hz          = LEDC_FREQUENCY,  // Set output frequency at 5 kHz
+        .clk_cfg          = LEDC_AUTO_CLK
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+
+    // Prepare and then apply the LEDC PWM channel configuration
+    ledc_channel_config_t ledc_channel = {
+        .speed_mode     = LEDC_MODE,
+        .channel        = LEDC_CHANNEL,
+        .timer_sel      = LEDC_TIMER,
+        .intr_type      = LEDC_INTR_DISABLE,
+        .gpio_num       = LEDC_OUTPUT_IO,
+        .duty           = 0, // Set duty to 0%
+        .hpoint         = 0
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+}
+
+
+rcl_subscription_t subscriber;
+std_msgs__msg__Int32 msg;
+
+void subscription_callback(const void * msgin)
+{
+	const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
+    // Set duty to 50%
+    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, msg->data));
+    // Update duty to apply the new value
+    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+
+}
+
+void appMain(void * arg)
+{
+
+  // Set the LEDC peripheral configuration
+    example_ledc_init();
+    // Set duty to 50%
+    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
+    // Update duty to apply the new value
+    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+
+
+
+	rcl_allocator_t allocator = rcl_get_default_allocator();
+	rclc_support_t support;
+
+	// create init_options
+	RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+	// create node
+	rcl_node_t node;
+	RCCHECK(rclc_node_init_default(&node, "motor_controller", "", &support));
+
+	// create subscriber
+	RCCHECK(rclc_subscription_init_default(
+		&subscriber,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+		"motor_controller_subscriber"));
+
+	// create executor
+	rclc_executor_t executor;
+	RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+    RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
+
+	while(1){
+		rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+		usleep(100000);
+	}
+
+	// free resources
+	RCCHECK(rcl_subscription_fini(&subscriber, &node))
+	RCCHECK(rcl_node_fini(&node))
+
+  	vTaskDelete(NULL);
+}
